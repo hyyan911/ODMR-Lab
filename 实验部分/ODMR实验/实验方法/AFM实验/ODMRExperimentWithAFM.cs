50061c32e00adadab28e75f9bb9c84eace02c958
hy911814 <hy911814@outlook.com>
2025-03-02 10:34:20 +0800

    2025.3.2

--- 实验部分/ODMR实验/实验方法/AFM实验/ODMRExperimentWithAFM.cs
+++ 实验部分/ODMR实验/实验方法/AFM实验/ODMRExperimentWithAFM.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using ODMR_Lab.ODMR实验;
 using ODMR_Lab.实验部分.ODMR实验.实验方法.ScanCore;
+using ODMR_Lab.设备部分.位移台部分;
 using ODMR_Lab.设备部分.射频源_锁相放大器;
 
 namespace ODMR_Lab.实验部分.ODMR实验.实验方法.AFM
@@ -36,17 +37,64 @@ namespace ODMR_Lab.实验部分.ODMR实验.实验方法.AFM
         public override void PreExpEvent()
         {
             PreExpEventBeforeDropWithAFM();
-            SetExpState("正在下针...");
-            //下针
-            AFMDrop drop = new AFMDrop();
-            var result = drop.CoreMethod(new List<object>(), GetLockIn());
-            if ((bool)result[0] == false)
+            if (GetAllowMultiDrop())
             {
-                ///没下到针
-                throw new Exception("下针未完成,PID输出已经到达最大值");
+                //多次下针
+                NanoStageInfo samplez = GetSampleZ();
+                bool movereverse = GetSampleAxisReverse();
+                int maxDropLoop = GetMaxDropCount();
+                double dropDet = GetDropDet();
+                bool dropsucceed = false;
+                for (int i = 0; i < maxDropLoop; i++)
+                {
+                    SetExpState("正在下针...当前尝试次数:" + (i + 1).ToString());
+                    //下针
+                    AFMDrop drop = new AFMDrop();
+                    var result = drop.CoreMethod(new List<object>(), GetLockIn());
+                    if ((bool)result[0] == true)
+                    {
+                        dropsucceed = true;
+                        break;
+                    }
+                    JudgeThreadEndOrResumeAction?.Invoke();
+                    SetExpState("当前下针尝试失败，正在撤针...");
+                    //撤针，升高样品
+                    AFMStopDrop stdrop = new AFMStopDrop();
+                    stdrop.CoreMethod(new List<object>(), GetLockIn());
+                    JudgeThreadEndOrResumeAction?.Invoke();
+                    //升高位移台
+                    samplez.Device.MoveStepAndWait(dropDet * (movereverse ? -1 : 1), 3000);
+                }
+                if (!dropsucceed)
+                {
+                    ///没下到针
+                    throw new Exception("下针未完成,达到最大尝试次数");
+                }
+            }
+            else
+            {
+                SetExpState("正在下针...");
+                //下针
+                AFMDrop drop = new AFMDrop();
+                var result = drop.CoreMethod(new List<object>(), GetLockIn());
+                if ((bool)result[0] == false)
+                {
+                    ///没下到针
+                    throw new Exception("下针未完成,PID输出已经到达最大值");
+                }
             }
             PreExpEventWithAFM();
         }
+
+        protected abstract bool GetAllowMultiDrop();
+
+        protected abstract int GetMaxDropCount();
+
+        protected abstract double GetDropDet();
+
+        protected abstract bool GetSampleAxisReverse();
+
+        protected abstract NanoStageInfo GetSampleZ();
 
         /// <summary>
         /// AFM下针后,实验开始前要进行的操作
--- 实验部分/ODMR实验/实验方法/AFM实验/二维扫描/AFMScanExp.cs
+++ 实验部分/ODMR实验/实验方法/AFM实验/二维扫描/AFMScanExp.cs
@@ -32,6 +32,10 @@ namespace ODMR_Lab.实验部分.ODMR实验.实验方法.AFM
         {
             new Param<bool>("显示子实验窗口",true,"ShowSubMenu"),
             new Param<bool>("存储单点实验数据",true,"SaveSingleExpData"),
+            new Param<bool>("尝试多次下针",true,"MultiAFMDrop"),
+            new Param<int>("最大尝试下针次数",5,"DropCount"),
+            new Param<double>("尝试下针失败后样品移动量",0.005,"DropDet"),
+            new Param<bool>("样品轴升高方向反向",false,"SampleAxisReverse"),
         };
         public override List<ParamB> OutputParams { get; set; } = new List<ParamB>()
         {
@@ -43,6 +47,7 @@ namespace ODMR_Lab.实验部分.ODMR实验.实验方法.AFM
             new KeyValuePair<DeviceTypes, Param<string>>(DeviceTypes.AFM扫描台,new Param<string>("扫描台 X","","ScannerX")),
             new KeyValuePair<DeviceTypes, Param<string>>(DeviceTypes.AFM扫描台,new Param<string>("扫描台 Y","","ScannerY")),
             new KeyValuePair<DeviceTypes, Param<string>>(DeviceTypes.锁相放大器,new Param<string>("Lock In","","LockIn")),
+            new KeyValuePair<DeviceTypes, Param<string>>(DeviceTypes.样品位移台,new Param<string>("样品Z轴","","SampleZ")),
         };
         public override List<ODMRExpObject> SubExperiments { get; set; } = new List<ODMRExpObject>();
 
@@ -217,6 +222,30 @@ namespace ODMR_Lab.实验部分.ODMR实验.实验方法.AFM
             }
 
             return true;
+        }
+
+        protected override bool GetAllowMultiDrop()
+        {
+            return GetInputParamValueByName("MultiAFMDrop");
+        }
+
+        protected override int GetMaxDropCount()
+        {
+            return GetInputParamValueByName("DropCount");
+        }
+
+        protected override double GetDropDet()
+        {
+            return GetInputParamValueByName("DropDet");
+        }
+
+        protected override bool GetSampleAxisReverse()
+        {
+            return GetInputParamValueByName("SampleAxisReverse");
+        }
+        protected override NanoStageInfo GetSampleZ()
+        {
+            return GetDeviceByName("SampleZ") as NanoStageInfo;
         }
     }
 }
